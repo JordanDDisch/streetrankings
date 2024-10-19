a142ead1f3955fd42c2b9f82c98d04fe41320e2a
Jordan Disch <jordandisch@Jordans-MacBook-Pro.local>
2024-10-20 09:41:30 +1100

    custom callback url

--- src/app/actions/login.ts
+++ src/app/actions/login.ts
@@ -6,5 +6,5 @@ const currentEnv = process.env.NODE_ENV
 export async function login() {
   const clientId = process.env.LIGHTROOM_CLIENT_ID;
 
-  redirect(`https://ims-na1.adobelogin.com/ims/authorize?${currentEnv === 'development' ? 'redirect_uri=https://localhost:3000/api/lightroom-callback&' : ''}client_id=${clientId}&scope=openid,AdobeID,lr_partner_apis,offline_access&response_type=code`);
+  redirect(`https://ims-na1.adobelogin.com/ims/authorize?${currentEnv === 'development' ? 'redirect_uri=https://localhost:3000/api/lightroom-callback' : 'https://www.streetrankings.com/api/lightroom-callback'}&client_id=${clientId}&scope=openid,AdobeID,lr_partner_apis,offline_access&response_type=code`);
 }
