73900cd3032e1ce74de1b26dfbae04eb6f3df165
Jordan Disch <jordandisch@Jordans-MacBook-Pro.local>
2024-10-20 09:32:15 +1100

    stuff

--- src/app/actions/login.ts
+++ src/app/actions/login.ts
@@ -1,8 +1,10 @@
 'use server'
 import { redirect } from 'next/navigation'
 
+const currentEnv = process.env.NODE_ENV
+
 export async function login() {
   const clientId = process.env.LIGHTROOM_CLIENT_ID;
 
-  redirect(`https://ims-na1.adobelogin.com/ims/authorize?redirect_uri=https://localhost:3000/api/lightroom-callback&client_id=${clientId}&scope=openid,AdobeID,lr_partner_apis,offline_access&response_type=code`);
+  redirect(`https://ims-na1.adobelogin.com/ims/authorize?${currentEnv === 'development' ? 'redirect_uri=https://localhost:3000/api/lightroom-callback&' : ''}client_id=${clientId}&scope=openid,AdobeID,lr_partner_apis,offline_access&response_type=code`);
 }
